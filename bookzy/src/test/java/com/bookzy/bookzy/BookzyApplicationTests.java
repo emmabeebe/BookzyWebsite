f2d4b1e21020cf2db925f27e4e4e2a7c8af4ceba
student <emma.beebe@ngc.com>
2021-02-19 07:31:12 +0000

    Add logic and test and deploy

--- bookzy/src/test/java/com/bookzy/bookzy/BookzyApplicationTests.java
+++ bookzy/src/test/java/com/bookzy/bookzy/BookzyApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bookzy.bookzy;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookzyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private BookzyApplication ticketer = new BookzyApplication();
+    
+    @Test
+    public void testRandomNumber() {
+   	 assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);
+    }
 
 }
